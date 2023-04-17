f0ebf9e7be44d18df436c9450f3560c3b0c0975d
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-04-17 18:49:36 +0100

    add telescope-file-browser

--- lua/lukep/packer.lua
+++ lua/lukep/packer.lua
@@ -65,4 +65,8 @@ return require('packer').startup(function(use)
             "nvim-telescope/telescope.nvim"
         }
     })
+    use {
+        "nvim-telescope/telescope-file-browser.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    }
 end)
