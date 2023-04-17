e36306deda7ccf83ff3865f5ac41fd5c59fa651e
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-04-17 12:43:01 +0100

    add toggleterm config

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,43 @@
+require("toggleterm").setup {
+  shade_terminals = false
+}
+
+vim.keymap.set('n', '<leader>t', vim.cmd.ToggleTermToggleAll)
+vim.keymap.set('n', '<leader>t1', function() vim.cmd("ToggleTerm size=30") end)
+vim.keymap.set('n', '<leader>t2', function() vim.cmd("2ToggleTerm size=30") end)
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- Lazygit 
+local Terminal  = require('toggleterm.terminal').Terminal
+
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
