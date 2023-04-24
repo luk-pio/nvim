0be335cb840be770a582a66bdf77c7a0a697dc94
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-04-24 19:20:04 +0100

    add more terminals

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,23 +1,27 @@
-require("toggleterm").setup {
-  shade_terminals = false
-}
+require("toggleterm").setup({
+  shade_terminals = false,
+  shading_factor = '0',
+  winbar = { enabled = false }
+})
 
 vim.keymap.set('n', '<leader>t', vim.cmd.ToggleTermToggleAll)
 vim.keymap.set('n', '<leader>t1', function() vim.cmd("ToggleTerm size=30") end)
 vim.keymap.set('n', '<leader>t2', function() vim.cmd("2ToggleTerm size=30") end)
+vim.keymap.set('n', '<leader>t3', function() vim.cmd("3ToggleTerm size=30") end)
+vim.keymap.set('n', '<leader>t4', function() vim.cmd("4ToggleTerm size=30") end)
 
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
+  local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
--- Lazygit 
-local Terminal  = require('toggleterm.terminal').Terminal
+-- Lazygit
+local Terminal = require('toggleterm.terminal').Terminal
 
-local lazygit = Terminal:new({
+local lazygit  = Terminal:new({
   cmd = "lazygit",
   dir = "git_dir",
   direction = "float",
@@ -27,7 +31,7 @@ local lazygit = Terminal:new({
   -- function to run on opening the terminal
   on_open = function(term)
     vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
   end,
   -- function to run on closing the terminal
   on_close = function(term)
@@ -39,4 +43,4 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
