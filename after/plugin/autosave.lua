83653652246eaad9240407d2c553828d757316af
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-05-13 12:06:09 +0100

    disable autosave

--- after/plugin/autosave.lua
+++ after/plugin/autosave.lua
@@ -1,8 +1,8 @@
-local function autosave_on_buffer_leave()
-  vim.cmd("augroup autosave_on_buffer_leave")
-  vim.cmd("autocmd!")
-  vim.cmd("autocmd BufLeave * silent! write")
-  vim.cmd("augroup END")
-end
+-- local function autosave_on_buffer_leave()
+--   vim.cmd("augroup autosave_on_buffer_leave")
+--   vim.cmd("autocmd!")
+--   vim.cmd("autocmd BufLeave * silent! write")
+--   vim.cmd("augroup END")
+-- end
 
-autosave_on_buffer_leave()
+-- autosave_on_buffer_leave()
