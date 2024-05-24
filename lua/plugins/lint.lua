b937d82f7d10d55cdc705bfa6b801a678310a4e6
lpiotrak <lpiotrak@amazon.com>
2024-05-24 23:54:38 +0100

    feat: better command line

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -13,15 +13,6 @@ return {
 			python = { "pylint" },
 		}
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
-
 		vim.keymap.set("n", "<leader>ll", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
