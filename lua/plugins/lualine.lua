2d662e0159d329ba2860b6998ea5f8ef3efa5c06
lpiotrak <lpiotrak@amazon.com>
2024-05-25 08:34:56 +0100

    chore: rounded separators lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,25 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require("lualine").setup({
-      options = { theme = "auto" },
-      extensions = { "toggleterm", "nvim-tree" },
-      sections = {
-        lualine_b = {
-          { "branch", "diff", "diagnostics" },
-        },
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_x = { "encoding", "filetype" },
-      },
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				section_separators = { left = "", right = "" },
+			},
+			extensions = { "toggleterm", "nvim-tree" },
+			sections = {
+				lualine_b = {
+					{ "branch", "diff", "diagnostics" },
+				},
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = { "encoding", "filetype" },
+			},
+		})
+	end,
 }
