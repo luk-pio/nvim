11e8a83086c8b1503480145b74377e6d7dbab0a6
lpiotrak <lpiotrak@amazon.com>
2024-05-26 20:20:38 +0100

    chore: add leap highlights

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,8 +1,11 @@
 return {
-  'ggandor/leap.nvim',
-  config = function()
-    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
-  end
+	"ggandor/leap.nvim",
+	config = function()
+		require("leap").setup({
+			highlight_unlabeled_phase_one_targets = true,
+		})
+		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+		vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+		vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
+	end,
 }
