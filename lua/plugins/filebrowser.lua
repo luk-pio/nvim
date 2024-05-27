06cae03c7a0d749f78a22b1feb19053ed24448c5
lpiotrak <lpiotrak@amazon.com>
2024-05-27 08:09:06 +0100

    chore: change filebrowser keymap

--- lua/plugins/filebrowser.lua
+++ lua/plugins/filebrowser.lua
@@ -5,6 +5,6 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup()
-		vim.keymap.set("n", "<leader>fh", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
