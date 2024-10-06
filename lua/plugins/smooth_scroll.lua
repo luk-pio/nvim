b48448bd8c2230dcea35f72fe76fd67140b0af14
lpiotrak <lpiotrak@amazon.com>
2024-10-06 11:51:02 +0100

    fix: remove deprecated smoothscroll options

--- lua/plugins/smooth_scroll.lua
+++ lua/plugins/smooth_scroll.lua
@@ -2,12 +2,10 @@ return {
 	"declancm/cinnamon.nvim",
 	config = function()
 		require("cinnamon").setup({
-			default_keymaps = true,
-			extra_keymaps = true,
-			extended_keymaps = true,
-			override_keymaps = true,
-			default_delay = 2,
-			max_length = 60,
+			keymaps = { extra = true },
+			options = {
+				delay = 2,
+			},
 		})
 	end,
 }
